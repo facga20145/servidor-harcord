88435969c65e50e97a79a112470102fa89c64e74
Facundo Godoy Mejia <66143994+facga20145@users.noreply.github.com>
2024-08-03 22:45:12 +0000

    Mundo guardado 2024-08-03

--- server.py
+++ server.py
@@ -10,7 +10,7 @@
 #-----------      ---------------------------
 # ap	          Asia / Pacífico (Singapore)
 # au		      Australia (Sydney)
-# eu		      Europa (Frankfurt)
+# eu		      Etpuropa (Frankfurt)
 # in		      India (Mumbai)
 # jp		      Japón (Tokyo)
 # sa		      Sudamérica (São Paulo)
